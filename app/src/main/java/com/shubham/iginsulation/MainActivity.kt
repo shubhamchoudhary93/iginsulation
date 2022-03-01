31c2320fbc82c42a851749999d705519654d9cc8
shubhamchoudhary <shubhamchoudhary@outlook.com>
2022-03-01 19:08:35 +0530

    update list 1. Fixed Back Navigation 2. Fixed Top Action Bar

--- app/src/main/java/com/shubham/iginsulation/MainActivity.kt
+++ app/src/main/java/com/shubham/iginsulation/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.shubham.iginsulation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
